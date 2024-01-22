351b29e76ab0ab6fe0a327c1aef7fa79e02d59f0
Pawel <email@gmail.com>
2024-01-22 18:15:32 +0100

    Adding login_page view logic

--- users_app/views.py
+++ users_app/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render, redirect
 from .forms import UserLoginForm, UserRegisterForm
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
 
 def login_page(request):
 
     template = "login.html"
     form = UserLoginForm
+
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("")
 
     return render(request, template, {"form": form})
 
