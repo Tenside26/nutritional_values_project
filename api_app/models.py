886e026e717ce990334daadf96a4f1a560c7e0ed
Pawel <email@gmail.com>
2024-01-25 19:20:22 +0100

    Fixing mistake, creating serializers.py file and there creating CustomUser serializer

--- api_app/models.py
+++ api_app/models.py
@@ -1,8 +1,1 @@
-from rest_framework import serializers
-from users_app.models import CustomUser
-
-
-class ItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = "username", "first_name", "last_name", "email"
+from django.db import models
--- api_app/views.py
+++ api_app/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+from users_app.models import CustomUser
+from .serializers import ItemSerializer
+
+
+class CustomUserListView(generics.ListCreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = ItemSerializer
