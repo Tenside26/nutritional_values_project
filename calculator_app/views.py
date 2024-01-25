26158514b63e87c0dfd62a88fadbc593c4635247
Pawel <email@gmail.com>
2024-01-25 18:42:17 +0100

    Creating calculator_page view with access to api with fake data

--- calculator_app/views.py
+++ calculator_app/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponseServerError
+import requests
+
 
 def calculator_page(request):
 
     template = "calculator.html"
+    api_url = "https://jsonplaceholder.typicode.com/posts"
 
-    return render(request, template, {})
+    try:
+        response = requests.get(api_url)
+
+        if response.status_code == 200:
+            api_data = response.json()
+
+            return render(request, template, {'api_data': api_data})
+
+    except requests.exceptions.RequestException as e:
+
+        return HttpResponseServerError(f"Error fetching data from API: {e}")
+
+    except ValueError as e:
+
+        return HttpResponseServerError(f"Error parsing JSON data: {e}")
+
+    except Exception as e:
+
+        return HttpResponseServerError(f"An unexpected error occurred: {e}")
