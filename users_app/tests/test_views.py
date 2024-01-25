4dc369e62a62ba6542cde6088be4ad5a999a204d
Pawel <email@gmail.com>
2024-01-25 17:50:15 +0100

    Adding tests to check if forms renders correctly in login_page and register_page

--- users_app/tests/test_views.py
+++ users_app/tests/test_views.py
@@ -1,43 +1,42 @@
 from django.test import TestCase
-from users_app.models import CustomUser
-from django.urls import reverse
-from users_app.forms import UserLoginForm
+from django.urls import reverse, resolve
+from users_app.forms import UserLoginForm, UserRegisterForm
+from users_app.views import login_page, register_page
 
 
 class LoginPageTest(TestCase):
     def setUp(self):
         self.login_url = reverse('login')
-        self.user = CustomUser.objects.create_user(username='test_user', password='test_password')
-
-    def tearDown(self):
-        self.user.delete()
 
     def test_login_page_template_render_correctly(self):
         response = self.client.get(self.login_url)
 
         self.assertTemplateUsed(response,'login.html')
 
+    def test_login_page_url(self):
+        found = resolve('/')
+        self.assertEqual(found.func, login_page)
+
     def test_login_page_renders_correct_form(self):
-        response = self.client.get(self.login_url)
+        response = self.client.post(self.login_url)
 
         self.assertIsInstance(response.context['form'], UserLoginForm)
-        self.assertContains(response, '<form method="post" action="{}">'.format(self.login_url), html=True)
-        self.assertContains(response, 'csrfmiddlewaretoken', html=True)
 
 
 class RegisterPageTest(TestCase):
     def setUp(self):
         self.register_url = reverse('register')
-        self.user = CustomUser.objects.create_user(username='test_user',
-                                                   password='test_password',
-                                                   first_name='test_first',
-                                                   last_name='test_last',
-                                                   email='test@example.com')
-
-    def tearDown(self):
-        self.user.delete()
 
     def test_register_page_template_render_correctly(self):
         response = self.client.get(self.register_url)
 
         self.assertTemplateUsed(response,'register.html')
+
+    def test_register_page_url(self):
+        found = resolve('/register')
+        self.assertEqual(found.func, register_page)
+
+    def test_register_page_renders_correct_form(self):
+        response = self.client.post(self.register_url)
+
+        self.assertIsInstance(response.context['form'], UserRegisterForm)
