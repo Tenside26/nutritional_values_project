512ea333f2a2883dd5c58bbdf5ea69f730f13b35
Pawel <email@gmail.com>
2024-02-01 17:34:34 +0100

    Creating ProductListView and linking it to urls.py

--- api_app/urls.py
+++ api_app/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path
-from .views import CustomUserListView
+from .views import CustomUserListView, ProductListView
 
 urlpatterns = [
     path('users-list/', CustomUserListView.as_view(), name='api-users-list'),
+    path('products-list/', ProductListView.as_view(), name='api-products-list'),
 ]
--- api_app/views.py
+++ api_app/views.py
@@ -1,12 +1,17 @@
-
+from calculator_app.models import Product
 from rest_framework import generics
 from users_app.models import CustomUser
-from .serializers import CustomUserSerializer
+from .serializers import CustomUserSerializer, ProductSerializer
 
 
 class CustomUserListView(generics.ListCreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
+
+
+class ProductListView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
 
 
 
