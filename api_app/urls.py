2ca58d507339708893eb39d8aca012262dc53eab
Pawel <email@gmail.com>
2024-02-01 17:54:01 +0100

    Creating ProductDetailView and linking it to urls.py

--- api_app/urls.py
+++ api_app/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path
-from .views import CustomUserListView, ProductListView
+from .views import CustomUserListView, ProductListView, ProductDetailView
 
 urlpatterns = [
     path('users-list/', CustomUserListView.as_view(), name='api-users-list'),
     path('products-list/', ProductListView.as_view(), name='api-products-list'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='api-product-detail'),
 ]
--- api_app/views.py
+++ api_app/views.py
@@ -1,5 +1,6 @@
 from calculator_app.models import Product
 from rest_framework import generics
+from rest_framework.generics import RetrieveAPIView
 from users_app.models import CustomUser
 from .serializers import CustomUserSerializer, ProductSerializer
 from rest_framework.pagination import PageNumberPagination
@@ -21,5 +22,7 @@ class ProductListView(generics.ListCreateAPIView):
     pagination_class = ProductsPageNumberPagination
 
 
-
+class ProductDetailView(RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
 
