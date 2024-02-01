5ebe802f5adc3370404737d56ce76352714c5e03
Pawel <email@gmail.com>
2024-02-01 17:36:52 +0100

    Adding pagination for ProductListView

--- api_app/views.py
+++ api_app/views.py
@@ -2,6 +2,12 @@ from calculator_app.models import Product
 from rest_framework import generics
 from users_app.models import CustomUser
 from .serializers import CustomUserSerializer, ProductSerializer
+from rest_framework.pagination import PageNumberPagination
+
+
+class ProductsPageNumberPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
 
 
 class CustomUserListView(generics.ListCreateAPIView):
@@ -12,6 +18,7 @@ class CustomUserListView(generics.ListCreateAPIView):
 class ProductListView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    pagination_class = ProductsPageNumberPagination
 
 
 
