c77f10c5d3b0a7b899b5552e98f2c20ac5e2aaf0
Pawel <email@gmail.com>
2024-02-05 17:22:06 +0100

    Adding test_user_deserialization test

--- api_app/tests/test_serializers.py
+++ api_app/tests/test_serializers.py
@@ -14,12 +14,20 @@ class CustomUserSerializerTests(TestCase):
             'last_name': self.fake.last_name(),
             'email': self.fake.email(),
         }
+        self.serializer_input_data = {
+            'username': self.fake.user_name(),
+            'first_name': self.fake.first_name(),
+            'last_name': self.fake.last_name(),
+            'email': self.fake.email(),
+        }
         self.user_instance = CustomUser.objects.create(**self.user_data)
         self.serializer = CustomUserSerializer(instance=self.user_instance)
 
     def tearDown(self):
         if self.user_instance:
             self.user_instance.delete()
+        if hasattr(self, 'created_instance'):
+            self.created_instance.delete()
         super().tearDown()
 
     def test_user_serialization(self):
@@ -29,3 +37,17 @@ class CustomUserSerializerTests(TestCase):
         self.assertEqual(serialized_data['first_name'], self.user_data['first_name'])
         self.assertEqual(serialized_data['last_name'], self.user_data['last_name'])
         self.assertEqual(serialized_data['email'], self.user_data['email'])
+
+    def test_user_deserialization(self):
+
+        serializer = CustomUserSerializer(data=self.serializer_input_data)
+        self.assertTrue(serializer.is_valid())
+
+        instance = serializer.save()
+        self.created_instance = instance
+        saved_instance = CustomUser.objects.get(pk=instance.pk)
+
+        self.assertEqual(saved_instance.username, self.serializer_input_data['username'])
+        self.assertEqual(saved_instance.first_name, self.serializer_input_data['first_name'])
+        self.assertEqual(saved_instance.last_name, self.serializer_input_data['last_name'])
+        self.assertEqual(saved_instance.email, self.serializer_input_data['email'])
