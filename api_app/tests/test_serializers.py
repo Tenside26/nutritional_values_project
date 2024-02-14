5bf6cfeea6d538bdd17523d02ed551f21c4cb0be
Pawel <email@gmail.com>
2024-02-14 19:39:24 +0100

    Adding test_product_serialization_wrong_fat

--- api_app/tests/test_serializers.py
+++ api_app/tests/test_serializers.py
@@ -192,3 +192,13 @@ class ProductSerializerTests(TestCase):
                                                                                positive=True))
         self.assertEqual(serialized_data['fat'], self.product_data.fat)
 
+    def test_product_serialization_wrong_fat(self):
+        serialized_data = self.serializer.data
+
+        self.assertEqual(serialized_data['name'],  self.product_data.name)
+        self.assertEqual(serialized_data['serving_size'], self.product_data.serving_size)
+        self.assertEqual(serialized_data['calories'], self.product_data.calories)
+        self.assertEqual(serialized_data['protein'], self.product_data.protein)
+        self.assertEqual(serialized_data['carbohydrate'], self.product_data.carbohydrate)
+        self.assertNotEqual(serialized_data['fat'], self.fake.pyfloat(left_digits=2, right_digits=2, positive=True))
+
