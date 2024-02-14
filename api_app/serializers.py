2c2e30b121ea17893e3877bbf2e34933b1098e0f
Pawel <email@gmail.com>
2024-02-14 17:46:41 +0100

    Fixing fields in models.py and creating MealSerializer

--- api_app/serializers.py
+++ api_app/serializers.py
@@ -1,15 +1,25 @@
 from rest_framework import serializers
 from users_app.models import CustomUser
-from calculator_app.models import Product
+from calculator_app.models import Product, Meal
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = CustomUser
-        fields = "pk", "username", "first_name", "last_name", "email"
+        fields = ("pk", "username", "first_name", "last_name", "email")
 
 
 class ProductSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Product
-        fields = "__all__"
+        fields = ("pk", "name", "serving_size", "calories", "protein", "carbohydrate", "fat")
+
+
+class MealSerializer(serializers.ModelSerializer):
+    products = ProductSerializer(many=True)
+
+    class Meta:
+        model = Meal
+        fields = ("pk", "user", "product")
--- calculator_app/models.py
+++ calculator_app/models.py
@@ -4,8 +4,8 @@ from users_app.models import CustomUser
 
 class Product(models.Model):
     name = models.CharField(max_length=255, unique=True, blank=False, null=False)
-    serving_size = models.IntegerField(default=100, max_length=25, blank=False, null=False)
-    calories = models.IntegerField(max_length=25)
+    serving_size = models.IntegerField(default=100, blank=False, null=False)
+    calories = models.IntegerField()
     protein = models.DecimalField(max_digits=15, decimal_places=2)
     carbohydrate = models.DecimalField(max_digits=15, decimal_places=2)
     fat = models.DecimalField(max_digits=15, decimal_places=2)
