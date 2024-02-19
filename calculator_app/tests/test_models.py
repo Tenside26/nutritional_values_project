619b7caf3f0699e7590bd75fdafab0f33005bf41
Pawel <email@gmail.com>
2024-02-19 19:21:06 +0100

    Adding test_product_model_name_max_length

--- calculator_app/tests/test_models.py
+++ calculator_app/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from calculator_app.factories import ProductFactory
 from calculator_app.models import Product
+from django.core.exceptions import ValidationError
 
 
 class ProductModelTests(TestCase):
@@ -8,7 +9,7 @@ class ProductModelTests(TestCase):
     def setUp(self):
         self.product = ProductFactory()
 
-    def test_create_product(self):
+    def test_product_model_create_product(self):
         self.assertIsInstance(self.product, Product)
         self.assertIsNotNone(self.product.pk)
         self.assertIsNotNone(self.product.name)
@@ -17,3 +18,7 @@ class ProductModelTests(TestCase):
         self.assertIsNotNone(self.product.protein)
         self.assertIsNotNone(self.product.carbohydrate)
         self.assertIsNotNone(self.product.fat)
+
+    def test_product_model_name_max_length(self):
+        self.product.name = 'a' * 256
+        self.assertRaises(ValidationError, self.product.full_clean)
