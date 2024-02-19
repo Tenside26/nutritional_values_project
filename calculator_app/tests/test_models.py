df378ff48f14c6c2eaebd56b84c40b9641365f8e
Pawel <email@gmail.com>
2024-02-19 19:07:49 +0100

    Adding test_create_product

--- calculator_app/tests/test_models.py
+++ calculator_app/tests/test_models.py
@@ -1,5 +1,19 @@
 from django.test import TestCase
+from calculator_app.factories import ProductFactory
+from calculator_app.models import Product
 
 
 class ProductModelTests(TestCase):
-    pass
+
+    def setUp(self):
+        self.product = ProductFactory()
+
+    def test_create_product(self):
+        self.assertIsInstance(self.product, Product)
+        self.assertIsNotNone(self.product.pk)
+        self.assertIsNotNone(self.product.name)
+        self.assertIsNotNone(self.product.serving_size)
+        self.assertIsNotNone(self.product.calories)
+        self.assertIsNotNone(self.product.protein)
+        self.assertIsNotNone(self.product.carbohydrate)
+        self.assertIsNotNone(self.product.fat)
