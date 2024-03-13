ff924aef4c352a9c5236c8c72148454e70d21f03
Pawel <email@gmail.com>
2024-03-13 17:59:29 +0100

    Creating signals.py and registering it

--- calculator_app/apps.py
+++ calculator_app/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class CalculatorAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'calculator_app'
+
+    def ready(self):
+        import calculator_app.signals
