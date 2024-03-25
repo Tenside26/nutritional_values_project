36a3710bc89ce64c6446dd36339229a4253b3f2e
Pawel <email@gmail.com>
2024-03-25 17:40:11 +0100

    Adding new function create_user_modified_product to services

--- calculator_app/services.py
+++ calculator_app/services.py
@@ -1,13 +1,15 @@
 from django.db.models import Sum
+from api_app.serializers import UserModifiedProductSerializer
 
 
 def calculate_nutritional_values_per_serving_size(instance):
-    modified_serving_size = instance.serving_size / instance.product.serving_size
-    instance.name = instance.product.name
-    instance.calories = instance.product.calories * modified_serving_size
-    instance.protein = instance.product.protein * modified_serving_size
-    instance.carbohydrate = instance.product.carbohydrate * modified_serving_size
-    instance.fat = instance.product.fat * modified_serving_size
+    if instance:
+        modified_serving_size = instance.serving_size / instance.product.serving_size
+        instance.name = instance.product.name
+        instance.calories = instance.product.calories * modified_serving_size
+        instance.protein = instance.product.protein * modified_serving_size
+        instance.carbohydrate = instance.product.carbohydrate * modified_serving_size
+        instance.fat = instance.product.fat * modified_serving_size
 
 
 def sum_meal_nutritional_values(instance):
@@ -22,3 +24,16 @@ def sum_meal_nutritional_values(instance):
     instance.total_protein = aggregated_values['total_protein']
     instance.total_carbohydrate = aggregated_values['total_carbohydrate']
     instance.total_fat = aggregated_values['total_fat']
+
+
+def create_user_modified_product(request_data, meal_id):
+    request_data["meal"] = meal_id
+    serializer = UserModifiedProductSerializer(data=request_data)
+
+    if serializer.is_valid():
+        serializer.save()
+        calculate_nutritional_values_per_serving_size(serializer.instance)
+        sum_meal_nutritional_values(serializer.instance.meal)
+    else:
+        errors = serializer.errors
+        return errors
